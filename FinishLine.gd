f65119bb916f3a622dfd6ea854edc6f3425ddef2
Ifetim <57069786+Ifetim@users.noreply.github.com>
2023-06-16 10:24:08 -0600

    friday

--- FinishLine.gd
+++ FinishLine.gd
@@ -1,21 +1,41 @@
 extends Area2D
 
-onready var finish_area := $world/FinishLine
+onready var finish_area := get_node("/root/world/FinishLine")
+onready var player := get_node("/root/world/Player")
 
 
 
 func _ready() -> void:
-	#...
+	finish_area.connect("body_entered", self, "_on_FinishLine_body_entered")
 
-	finish_area.connect("body_entered", self, "_on_Finish_body_entered")
+
+
 
 
 
 func finish_game() -> void:
-
 	set_process(false)
+	player.set_physics_process(false)
+	get_tree().change_scene("res://world2.tscn")
+	
+# We can't directly connect the body_entered signal to the finish_game()
 
-	godot.set_physics_process(false)
+# function because the body_entered signal requires a callback with a `body`
+
+# parameter.
+
+# That's why we need this function.
+func _on_FinishLine_body_entered(body):
+	print(body)
+	if "Player" in body.name:
+		finish_game()
+
+
+
+
+
+
+
 
 
 # We can't directly connect the body_entered signal to the finish_game()
@@ -24,8 +44,11 @@ func finish_game() -> void:
 
 # parameter.
 
-#
-
 # That's why we need this function.
-func _on_FinishLine_body_entered(body):
-	.finish_game()
+
+
+
+
+
+
+
